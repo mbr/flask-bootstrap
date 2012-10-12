e34209b6714e87c888596610476ac0bfbec28c6a
Wouter Bolsterlee <uws@xs4all.nl>
2012-10-13 00:14:24 +0200

    Issue #8: do not store blueprint in init_app()

--- flask_bootstrap/__init__.py
+++ flask_bootstrap/__init__.py
@@ -53,14 +53,14 @@ class Bootstrap(object):
             '//netdna.bootstrapcdn.com/twitter-bootstrap/2.1.0/'
         )
 
-        self.blueprint = Blueprint(
+        blueprint = Blueprint(
             'bootstrap',
             __name__,
             template_folder='templates',
             static_folder='static',
             static_url_path=app.static_url_path + '/bootstrap')
 
-        app.register_blueprint(self.blueprint)
+        app.register_blueprint(blueprint)
 
         app.jinja_env.filters['bootstrap_is_hidden_field'] =\
             is_hidden_field_filter
