5e059145a3780ec30615ea4831d0e195fb7332af
Marc Brinkmann <git@marcbrinkmann.de>
2013-02-15 21:38:04 +0100

    Release 2.3.0-2.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='Flask-Bootstrap',
-    version='2.3.0-2dev',
+    version='2.3.0-2',
     url='http://github.com/mbr/flask-bootstrap',
     license='BSD',
     author='Marc Brinkmann.de',
