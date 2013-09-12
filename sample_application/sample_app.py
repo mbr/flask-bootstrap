9de82d84c5f0ecc9bbbf7e7e41912e51bc3ba997
Marc Brinkmann <git@marcbrinkmann.de>
2013-09-12 17:25:57 +0200

    Added sample application.

---
+++ sample_application/sample_app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_appconfig import AppConfig
+from flask_wtf import Form, RecaptchaField
+from wtforms import TextField, HiddenField, ValidationError, RadioField
+from wtforms.validators import Required
+
+
+class ExampleForm(Form):
+    field1 = TextField('First Field', description='This is field one.')
+    field2 = TextField('Second Field', description='This is field two.',
+                       validators=[Required()])
+    hidden_field = HiddenField('You cannot see this', description='Nope')
+    recaptcha = RecaptchaField('A sample recaptcha field')
+    radio_field = RadioField('This is a radio field', choices=[
+        ('head_radio', 'Head radio'),
+        ('radio_76fm', "Radio '76 FM"),
+        ('lips_106', 'Lips 106'),
+        ('wctr', 'WCTR'),
+    ])
+
+    def validate_hidden_field(form, field):
+        raise ValidationError('Always wrong')
+
+
+def create_app(configfile=None):
+    app = Flask(__name__)
+    AppConfig(app, configfile)  # Flask-Appconfig is not necessary, but
+                                # highly recommend =)
+                                # https://github.com/mbr/flask-appconfig
+    Bootstrap(app)
+
+    # in a real app, these should be configured through Flask-Appconfig
+    app.config['SECRET_KEY'] = 'devkey'
+    app.config['RECAPTCHA_PUBLIC_KEY'] = \
+        '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
+
+    @app.route('/')
+    def index():
+        form = ExampleForm()
+        return render_template('index.html', form=form)
+
+    return app
+
+if __name__ == '__main__':
+    create_app().run(debug=True)
