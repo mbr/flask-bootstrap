763ac7eda1acc467489fcd78870f2752ce7ff7fc
Marc Brinkmann <git@marcbrinkmann.de>
2013-11-20 14:45:47 +0100

    Increased html5shiv version to 3.7.

--- flask_bootstrap/__init__.py
+++ flask_bootstrap/__init__.py
@@ -115,7 +115,7 @@ class Bootstrap(object):
     def init_app(self, app):
         BOOTSTRAP_VERSION = re.sub(r'^(\d+\.\d+\.\d+).*', r'\1', __version__)
         JQUERY_VERSION = '2.0.3'
-        HTML5SHIV_VERSION = '3.6.2'
+        HTML5SHIV_VERSION = '3.7'
         RESPONDJS_VERSION = '1.3.0'
 
         app.config.setdefault('BOOTSTRAP_USE_MINIFIED', True)
