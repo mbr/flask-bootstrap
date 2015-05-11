7758daa74861318992af6ab3274b163fbdf221a1
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-11 10:47:02 +0200

    Add FileFiled to sample app.

--- sample_application/__init__.py
+++ sample_application/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
 from flask_appconfig import AppConfig
 from flask_wtf import Form, RecaptchaField
+from flask_wtf.file import FileField
 from wtforms import TextField, HiddenField, ValidationError, RadioField,\
     BooleanField, SubmitField, IntegerField, FormField, validators
 from wtforms.validators import Required
@@ -35,7 +36,10 @@ class ExampleForm(Form):
     # you can change the label as well
     office_phone = FormField(TelephoneForm, label='Your office phone')
 
+    ff = FileField('Sample upload')
+
     submit_button = SubmitField('Submit Form')
+
 
     def validate_hidden_field(form, field):
         raise ValidationError('Always wrong')
