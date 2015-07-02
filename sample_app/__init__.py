fe5dd01a3e139d7d6d69ba7d77dacfad87f1a492
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-02 15:37:25 +0200

    Added Flask-Debug.

--- sample_app/__init__.py
+++ sample_app/__init__.py
@@ -16,6 +16,7 @@
 from flask import Flask
 from flask_appconfig import AppConfig
 from flask_bootstrap import Bootstrap
+from flask_debug import Debug
 
 from .frontend import frontend
 
@@ -41,5 +42,8 @@ def create_app(configfile=None):
     # Because we're security-conscious developers, we also hard-code disabling
     # the CDN support (this might become a default in later versions):
     app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+    # For demonstration purposes, we also include Flask-Debug
+    Debug(app)
 
     return app
