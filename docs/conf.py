21a20c0365a49060a74ea9c3ac43feff7bdbf0fa
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-02 12:40:41 +0200

    Updated doc config.

--- docs/conf.py
+++ docs/conf.py
@@ -5,12 +5,35 @@ copyright = u'2013, Marc Brinkmann'
 version = '3.3.5.2'
 release = '3.3.5.2.dev1'
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'alabaster']
 source_suffix = '.rst'
 master_doc = 'index'
 exclude_patterns = ['_build']
-pygments_style = 'sphinx'
+pygments_style = 'monokai'
+
+
 html_theme = 'alabaster'
+html_theme_options = {
+    'github_user': 'mbr',
+    'github_repo': 'flask-bootstrap',
+    'description': 'All Bootstrap, no boilerplate.',
+    'github_banner': True,
+    'gratipay_user': 'mbr',
+    'github_button': False,
+    'show_powered_by': False,
+
+    # required for monokai:
+    'pre_bg': '#292429',
+}
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+        'donate.html',
+    ]
+}
 
 intersphinx_mapping = {'http://docs.python.org/': None,
                        'http://pythonhosted.org/Flask-SQLAlchemy/': None,
