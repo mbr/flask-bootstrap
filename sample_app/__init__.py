71b8eb5384ee51d827c4c0d6743a33480e156de6
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-03 12:41:03 +0200

    Added navigation to sample app.

--- sample_app/__init__.py
+++ sample_app/__init__.py
@@ -17,7 +17,7 @@ from flask import Flask
 from flask_appconfig import AppConfig
 from flask_bootstrap import Bootstrap
 
-from .frontend import frontend
+from .frontend import frontend, nav
 
 
 def create_app(configfile=None):
@@ -41,5 +41,8 @@ def create_app(configfile=None):
     # Because we're security-conscious developers, we also hard-code disabling
     # the CDN support (this might become a default in later versions):
     app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+    # We initialize the navigation as well
+    nav.init_app(app)
 
     return app
--- sample_app/frontend.py
+++ sample_app/frontend.py
@@ -6,9 +6,38 @@
 # http://flask.pocoo.org/docs/blueprints/
 
 from flask import Blueprint, render_template
-
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View, Subgroup, Link, Label, Separator
 
 frontend = Blueprint('frontend', __name__)
+
+# we're adding a navbar as well through flask-navbar
+nav = Nav()
+
+nav.register_element('top', Navbar(
+    View('Flask-Bootstrap', '.index'),
+    Subgroup(
+        'Docs',
+        Link('Flask-Bootstrap',
+             href='http://pythonhosted.org/Flask-Bootstrap'),
+        Link('Flask-AppConfig',
+             href='https://github.com/mbr/flask-appconfig'),
+        Link('Flask-Debug',
+             href='https://github.com/mbr/flask-debug'),
+        Separator(),
+        Label('Bootstrap'),
+        Link('Getting started',
+             href='http://getbootstrap.com/getting-started/'),
+        Link('CSS',
+             href='http://getbootstrap.com/css/'),
+        Link('Components',
+             href='http://getbootstrap.com/components/'),
+        Link('Javascript',
+             href='http://getbootstrap.com/javascript/'),
+        Link('Customize',
+             href='http://getbootstrap.com/customize/'),
+    )
+))
 
 
 # Our index-page just shows a quick explanation. Check out the template
