7072179e101a777ff2e017201783ae3b7f12f8fb
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-14 18:38:53 +0200

    Added missing nav.py of sample app.

---
+++ sample_app/nav.py
@@ -0,0 +1,7 @@
+from flask_nav import Nav
+
+# To keep things clean, we keep our Flask-Nav instance in here. We will define
+# frontend-specific navbars in the respective frontend, but it is also possible
+# to put share navigational items in here.
+
+nav = Nav()
