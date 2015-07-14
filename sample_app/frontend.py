e7cb095954ab72a6af49f4b718c767471492ea8d
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-14 14:36:25 +0200

    Fixed typo in comments.

--- sample_app/frontend.py
+++ sample_app/frontend.py
@@ -13,8 +13,8 @@ from .nav import nav
 frontend = Blueprint('frontend', __name__)
 
 # We're adding a navbar as well through flask-navbar. In our example, the
-# navbar has an usual amount of Link-Elements, more commonly you will a lot
-# more View instances.
+# navbar has an usual amount of Link-Elements, more commonly you will have a
+# lot more View instances.
 nav.register_element('frontend_top', Navbar(
     View('Flask-Bootstrap', '.index'),
     View('Home', '.index'),
