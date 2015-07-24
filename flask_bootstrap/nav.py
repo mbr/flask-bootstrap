81f8decc245c050dac99343a769bd5066fb40f41
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-24 12:03:07 +0200

    Fixed id to sha1 conversion (fixes #102).

--- flask_bootstrap/nav.py
+++ flask_bootstrap/nav.py
@@ -12,7 +12,7 @@ class BootstrapRenderer(Visitor):
     def visit_Navbar(self, node):
         # create a navbar id that is somewhat fixed, but do not leak any
         # information about memory contents to the outside
-        node_id = self.id or sha1(str(id(node))).hexdigest()
+        node_id = self.id or sha1(str(id(node)).encode()).hexdigest()
 
         root = tags.nav() if self.html5 else tags.div(role='navigation')
         root['class'] = 'navbar navbar-default'
