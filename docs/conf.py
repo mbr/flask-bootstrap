edf5a6764e97aef7df84435bf1a2db7bc2762d54
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-24 12:25:04 +0200

    Release version 3.3.5.5

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 project = u'Flask-Bootstrap'
 copyright = u'2013, Marc Brinkmann'
 version = '3.3.5.5'
-release = '3.3.5.5.dev1'
+release = '3.3.5.5'
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'alabaster']
 source_suffix = '.rst'
