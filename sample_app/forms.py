0200c03f8f6232965f924a765c5ebb0f9c439f4d
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-04 00:28:47 +0200

    Add email field to sample app.

--- sample_app/forms.py
+++ sample_app/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import Form
 from wtforms.fields import (TextField, SubmitField, BooleanField, DateField,
                             DateTimeField)
-from wtforms.validators import Required
+from wtforms.validators import Required, Email
 
 
 class SignupForm(Form):
     name = TextField(u'Your name', validators=[Required()])
+    email = TextField(u'Your email address', validators=[Email()])
     birthday = DateField(u'Your birthday')
     now = DateTimeField(u'Current time',
                         description='...for no particular reason')
