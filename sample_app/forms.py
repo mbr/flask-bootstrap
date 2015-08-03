61ca891778372bc0e970f6ce4f2ccce04cb1e685
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-04 00:15:15 +0200

    Made sample app for a little more interesting.

--- sample_app/forms.py
+++ sample_app/forms.py
@@ -1,7 +1,14 @@
 from flask_wtf import Form
-from wtforms.fields import TextField, SubmitField
+from wtforms.fields import (TextField, SubmitField, BooleanField, DateField,
+                            DateTimeField)
+from wtforms.validators import Required
 
 
 class SignupForm(Form):
-    name = TextField(u'Your name')
+    name = TextField(u'Your name', validators=[Required()])
+    birthday = DateField(u'Your birthday')
+    now = DateTimeField(u'Current time',
+                        description='...for no particular reason')
+    eula = BooleanField(u'I did not read the terms and conditions',
+                        validators=[Required('You must agree to not agree!')])
     submit = SubmitField(u'Signup')
