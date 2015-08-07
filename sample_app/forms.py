6d6b93b82d7d85f3784c9601c11bdc160a8a9c93
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-07 23:58:07 +0200

    Added sample_file field to sample application.

--- sample_app/forms.py
+++ sample_app/forms.py
@@ -7,11 +7,15 @@ class SignupForm(Form):
     name = TextField(u'Your name', validators=[Required()])
     email = TextField(u'Your email address', validators=[Email()])
     birthday = DateField(u'Your birthday')
+
     a_float = FloatField(u'A floating point number')
     a_decimal = DecimalField(u'Another floating point number')
     a_integer = IntegerField(u'An integer')
+
     now = DateTimeField(u'Current time',
                         description='...for no particular reason')
+    sample_file = FileField(u'Your favorite file')
     eula = BooleanField(u'I did not read the terms and conditions',
                         validators=[Required('You must agree to not agree!')])
+
     submit = SubmitField(u'Signup')
