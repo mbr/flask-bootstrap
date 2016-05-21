1b954433fddcde484b8fcd7af9ed531c8678ecbb
Marc Brinkmann <git@marcbrinkmann.de>
2016-05-21 16:59:11 +0200

    Bumped jquery version to 1.12.4.

--- flask_bootstrap/__init__.py
+++ flask_bootstrap/__init__.py
@@ -21,7 +21,7 @@ from .forms import render_form
 
 __version__ = '3.3.6.0.dev1'
 BOOTSTRAP_VERSION = re.sub(r'^(\d+\.\d+\.\d+).*', r'\1', __version__)
-JQUERY_VERSION = '1.11.3'
+JQUERY_VERSION = '1.12.4'
 HTML5SHIV_VERSION = '3.7.2'
 RESPONDJS_VERSION = '1.4.2'
 
