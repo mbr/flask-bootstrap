7a80a0305d8c1c165a955792fa961a13f8ccb6ba
Benjamin Bertrand <beenje@gmail.com>
2018-01-18 22:25:32 +0100

    Bump version to 4.0.0.0.dev1

--- flask_bootstrap/__init__.py
+++ flask_bootstrap/__init__.py
@@ -19,7 +19,7 @@ else:
 
 from .forms import render_form
 
-__version__ = '4.0.0-beta.3.dev1'
+__version__ = '4.0.0.0.dev1'
 BOOTSTRAP_VERSION_RE = re.compile(r'(\d+\.\d+\.\d+(\-[a-z]+)?)')
 POPPER_VERSION = '1.12.9'
 JQUERY_VERSION = '3.2.1'
